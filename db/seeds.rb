d0de2ed8690ec8cdc6921a7f0b2046d78a8b955d
FredHorizon <fredoliveira.sys@gmail.com>
2019-05-14 01:54:52 +0000

    Popula o banco com db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Esse arquivo deve conter toda a criação de registro necessária para popular o banco de dados com seus valores padrão.
+# Alguns sistemas já iniciam com algum registro populado como, por exemplo, os dados referentes ao estado e municípios.
+# rails db:seed tem essa finalidade de popular o banco com dados básicos ou dados para testes.
+# Os dados podem então ser carregados com o comando rails db: seed (ou criado ao lado do banco de dados com db: setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando moedas..."
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTH",
+    url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://banner2.kisspng.com/20180820/ryr/kisspng-ethereum-bitcoin-cryptocurrency-blockchain-logo--5b7b6e2bd92a00.5250630615348157878895.jpg"
+)
+
+Coin.create!(
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://www.ultimatemoney.com.au/wp-content/uploads/2018/06/dash-coin.png"
+)
+
+puts "Moedas cadastradas com sucesso!"
