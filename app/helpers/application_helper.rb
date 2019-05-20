a2b670db00f97f28c382b67cdefffe1026ecba15
FredHorizon <fredoliveira.sys@gmail.com>
2019-05-20 23:58:47 +0000

    Configura e ativa o i18n (2/2)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
     
+    def locale(locale)
+        # if I18n.locale == :en
+        #     "English USA"
+        # else
+        #     "Português Brasil"
+        # end
+
+        # if 'ternário' para sentenças simples:
+        I18n.locale == :en ? "English USA" : "Português Brasil"
+    end
+
     # Necessita da gem 'rails-i18n'
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
