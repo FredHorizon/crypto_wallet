fe2c6babe854699f2d5cc4e6dfdd00c7c50feec2
FredHorizon <fredoliveira.sys@gmail.com>
2019-05-22 18:10:11 +0000

    Testa cookies e sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso RoR 5.x [COOKIE]"
+    session[:curso] = "Curso RoR 5.x [SESSION]"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
